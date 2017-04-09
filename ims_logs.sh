ad466b1168a386a894d15c794ebdf55291a0e02b
Purandar <purandarrao@gmail.com>
2017-04-08 19:37:01 -0700

    Update ims_logs.sh

--- ims_logs.sh
+++ ims_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-log_path="/home/purandarrao/ims_v3/Logs/"
+log_path="<absolute_folder_path>"
 latest_folder=`ls -t $log_path | head -n1`
-log_filename="messages.log"
+log_filename="<log_filename>"
 log=$log_path$latest_folder/$log_filename
 echo $log
 tail -f $log
